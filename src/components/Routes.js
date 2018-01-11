d92010663b6287e8a6e172552c1aacc53db9374d
robin <robin.kath.wilson@gmail.com>
2018-01-11 10:28:31 -0800

    added scss compilers and css applying successfully to page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import '../css/index.scss';
+
 // Components
 import ChartSandbox from './ChartSandbox';
 import Charts from './Charts';
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,15 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ]
   },
 }
