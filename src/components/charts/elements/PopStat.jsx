040e6e6dfb9d07bac8c88c18dccc9852bfc3e89b
robin <robin.kath.wilson@gmail.com>
2018-01-11 12:10:40 -0800

    created components for chart functionality

---
+++ src/components/charts/elements/PopStat.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PopStat extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
---
+++ src/components/charts/elements/PopStatList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PopStatList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
