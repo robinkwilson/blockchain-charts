4ae491f8c3519becfc000a760f1fab994315e274
robin <robin.kath.wilson@gmail.com>
2018-01-12 19:52:58 -0800

    created toggling filters handled by chartlist state

--- src/components/charts/elements/ChartList.jsx
+++ src/components/charts/elements/ChartList.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+const filterList = ['#(Total BTC)', 'Time', '1 BTC', 'Total BTC', 'USD(Total BTC)']
+const filters = filterList.map(filterName => {
+  return {
+    text: filterName,
+    active: false
+  }
+})
+
 export default class ChartList extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      filters: [{ text: '#(Total BTC)', active: false }, 'Time', '1 BTC', 'Total BTC', 'USD(Total BTC)'],
+      filters: filters,
       activeFilters: [],
       currency_statistics: {
         charts: [
@@ -29,30 +37,48 @@ export default class ChartList extends Component {
       }
     }
 
-    []
-
-    this.toggle = this.bind(toggle);
   }
 
-  toggle(evt) {
-    evt.preventDefault();
+  toggleFilter(id, evt) {
 
+    const cur_id = id;
+    const cur_text = evt.target.innerText;
+    const cur_isActive = evt.target.className.indexOf('active') !== -1; 
+    let cur = this.state.filters[cur_id];
     
+    let active = this.state.activeFilters;
+    let filters = this.state.filters;
+
+    if(!cur_isActive){ // not active element
+      //add to activeFilters array
+      this.setState({activeFilters: active.concat([{text: evt.target.innerText, id: cur_id}])});
+    } else { // active element
+      //remove from activeFilters array
+      this.setState({activeFilters: active.filter(item => item.text !== cur_text)})
+    }
+    
+    // inside filters array toggle 'active' property
+    cur.active = !cur_isActive;
+    filters.splice(cur_id, 1, cur);
+    this.setState({filters: filters});  
   }
 
   render() {
     return (
-      <div style={{ backgroundColor: '#392' }}>
-        <h2>Filters</h2>
-        {
-          this.state.filters && this.state.filters.map((filter, id) => {
-            return (
-              <button key={id} type="button" className={filter.active ? "btn btn-primary focus active" : "btn btn-primary"} data-toggle="button" aria-pressed="false" autoComplete="off">
-                {filter.text}
-              </button>
-            );
-          })
-        }
+      <div>
+        <div style={{ backgroundColor: '#392' }}>
+          <h2>Filters</h2>
+          {
+            this.state.filters && this.state.filters.map((cur, id) => {
+              return (
+                <button key={id} type="button" onClick={this.toggleFilter.bind(this, id)} className={cur.active ? "btn btn-primary focus active" : "btn btn-primary"} data-toggle="button" aria-pressed="false" autoComplete="off">
+                  {cur.text}
+                </button>
+              );
+            })
+          }
+        </div>
+
       </div>
     );
   }
