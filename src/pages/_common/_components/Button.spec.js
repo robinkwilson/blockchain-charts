307bd29b6622d0c4ed5c3cf75bc188973a197827
robin <robin.kath.wilson@gmail.com>
2018-01-17 08:27:22 -0800

    testing environment set up, true equals true passes

---
+++ src/pages/_common/_components/Button.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+import chai, { expect } from 'chai';
+
+describe('Button common component', () => {
+
+  describe('should pass', () => {
+    it('should pass', () => {
+      expect(true).to.be.true;
+    })
+  })
+});
