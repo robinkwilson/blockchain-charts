e05fdcc227a99505460cdb7fc4dfa111bf513863
robin <robin.kath.wilson@gmail.com>
2018-01-17 09:57:53 -0800

    tested Button component, working on testing Link

--- src/pages/_common/_components/Button.spec.js
+++ src/pages/_common/_components/Button.spec.js
@@ -1,13 +1,68 @@
-import React from 'react';
+/* global describe beforeEach afterEach it */
 
-import { shallow } from 'enzyme';
+import React from 'react';
+import enzyme, { shallow } from 'enzyme';
 import chai, { expect } from 'chai';
+import adapter from 'enzyme-adapter-react-16';
+import TestUtils from 'react-dom/test-utils';
+
+
+enzyme.configure({ adapter: new adapter() });
+
+import Button from './Button.jsx';
 
 describe('Button common component', () => {
 
-  describe('should pass', () => {
-    it('should pass', () => {
-      expect(true).to.be.true;
+  describe('props: txt and classes', () => {
+    let wrapper;
+    let defaultProps;
+    beforeEach(() => {
+      defaultProps = {
+        classes: 'center padding-1',
+        txt: 'button text',
+      }
+      wrapper = shallow(<Button txt={defaultProps.txt} classes={defaultProps.classes} />);
+    });
+
+    it('render button only', () => {
+      expect(wrapper.find('button')).to.have.lengthOf(1);
+    });
+    it('txt as button text', () => {
+      expect(wrapper.find('button').text()).to.be.equal('button text');
+    });
+    it('props classes are button classes', () => {
+      expect(wrapper.find('button').hasClass('padding-1') && wrapper.find('button').hasClass('padding-1')).to.be.true;
+    });
+
+  });
+
+  describe('props: linkto, txt, and classes', () => {
+
+    let wrapper;
+    let defaultProps;
+    beforeEach(() => {
+      defaultProps = {
+        classes: 'center padding-1',
+        txt: 'button text',
+        linkto: 'page'
+      }
+      wrapper = shallow(<Button linkto={defaultProps.linkto} txt={defaultProps.txt} classes={defaultProps.classes} />);
+    });
+
+    it('render <a> with child <button>', () => {
+      expect(wrapper.find('Link').children('button')).to.have.length(1);
     })
-  })
+    xit('<a> with linkto as link', () => {
+      // Testing Link references 
+      // https://github.com/ReactTraining/react-router/blob/00c44b70ff0a06004df73b62acea837566c53256/modules/__tests__/Link-test.js#L41-L64
+      // https://stackoverflow.com/questions/35317371/testing-href-value-from-a-link-component-while-having-dynamic-props
+      // https://stackoverflow.com/questions/30332636/react-router-how-to-test-href-of-a-rendered-link
+      
+      TestUtils.renderIntoDocument(<Button linkto={defaultProps.linkto} txt={defaultProps.txt} classes={defaultProps.classes} />);
+      let a = TestUtils.findRenderedDOMComponentWithTag(document, 'a');
+      let domNode = a.getDOMNode();
+
+      expect(domNode.getAttribute('href')).to.equal('/page');
+    })
+  });
 });
